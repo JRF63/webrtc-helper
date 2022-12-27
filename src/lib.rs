170f440dea85240c5ae82ff3c677053b70ceb0ac
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2022-12-27 11:56:15 +0800

    JSON serde test

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub mod codecs;
 pub mod decoder;
 pub mod encoder;
 mod interceptor;
-mod peer;
+pub mod peer;
 pub mod signaling;
 pub mod util;
 
 #[cfg(test)]
 mod mock;
 
-pub use peer::WebRtcPeer;
+pub use peer::{WebRtcPeer, WebRtcBuilder};
--- src/signaling.rs
+++ src/signaling.rs
@@ -1,9 +1,9 @@
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
 use webrtc::{
     ice_transport::ice_candidate::RTCIceCandidate,
     peer_connection::sdp::session_description::RTCSessionDescription,
 };
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Message {
@@ -16,4 +16,24 @@ pub enum Message {
 pub trait Signaler: Send + Sync {
     async fn recv(&self) -> std::io::Result<Message>;
     async fn send(&self, msg: Message) -> std::io::Result<()>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn json_serde() {
+        let messages = [
+            Message::Sdp(RTCSessionDescription::default()),
+            Message::IceCandidate(RTCIceCandidate::default()),
+            Message::Bye,
+        ];
+        for message in messages {
+            let json = serde_json::to_string(&message).unwrap();
+            println!("{json}");
+            let _: Message = serde_json::from_str(&json).unwrap();
+        }
+        
+    }
 }
